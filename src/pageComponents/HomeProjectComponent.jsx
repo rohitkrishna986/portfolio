4a51b4ed56c7f8507c4d4df914ac58690300a853
rohitkrishna986 <rohitkrishna@gmail.com>
2024-08-05 13:56:49 +0530

    Updated

--- src/pageComponents/HomeProjectComponent.jsx
+++ src/pageComponents/HomeProjectComponent.jsx
@@ -3,7 +3,9 @@ import { FaTelegramPlane } from 'react-icons/fa'
 
 const HomeProjectComponent = () => {
   return (
-    <div className='hidden container mx-auto w-full animate-fade-in h-[300px] lg:flex flex-col justify-center items-center relative'>
+   <>
+    {/* // Desktop view */}
+    <div className='hidden container mx-auto w-full animate-fade-in h-[300px] lg:flex flex-col justify-center items-center'>
         <div>
             <h2 className='text-3xl font-bold'>
                 Project
@@ -15,6 +17,20 @@ const HomeProjectComponent = () => {
             <button className='flex items-center gap-2 bg-[rgb(20,20,20)] hover:bg-black text-white px-4 py-2 rounded-lg'>Projects<span className='text-xl'><FaTelegramPlane/></span></button>
         </div>
     </div>
+    {/* // Mobile view */}
+    <div className='md:hidden container mx-auto w-full animate-fade-in h-[300px] flex flex-col justify-center items-center'>
+        <div>
+            <h2 className='text-3xl font-bold'>
+                Project
+            </h2>
+        </div>
+        <div className='mt-8 flex flex-col justify-center items-center gap-4 px-5 text-justify'>
+            <p>Explore our cutting-edge projects and stay updated with the latest developments.
+            Click here to dive into our live project showcase and see the innovation in action!</p>
+            <button className='flex items-center gap-2 bg-[rgb(20,20,20)] hover:bg-black text-white px-4 py-2 rounded-lg'>Projects<span className='text-xl'><FaTelegramPlane/></span></button>
+        </div>
+    </div>
+   </>
   )
 }
 
